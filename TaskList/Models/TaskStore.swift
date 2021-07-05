0bc471dc35dc40a285f83b13af5c14bf15754af8
Mariana <mariana@fastmail.net>
2021-07-05 19:27:14 +0200

    json encoding, pretty format of json file on the disk

--- TaskList/Models/TaskStore.swift
+++ TaskList/Models/TaskStore.swift
@@ -1,8 +1,25 @@
 import Combine
 import Foundation
 
-class TaskStore: ObservableObject {    
-    @Published var prioritizedTasks: [PrioritizedTasks] = []
+class TaskStore: ObservableObject {
+    // get the URLs for each of the JSON files. For that you will use bundles URL
+    // Bundle is a representation of your app, its code and resources
+    // All the files you add in xcode to your app target are pakeged up into what is referred to as the app bundle
+    // main represents the current executable, in this case your app
+    // by asking your main bundle for the URL for a given resource and assuming it's located in the bundle, you will get back the URL where the file is located
+    // forResource: is the name of the file you want the URL for
+    // withExtension: is the file extension of the file
+    /// guard let tasksJSONURL = Bundle.main.url(forResource: "PrioritizedTasks", withExtension: "json") else { return }
+    
+    // or the another way:
+    let tasksJSONURL = URL(fileURLWithPath: "PrioritizedTasks", relativeTo: FileManager.documentsDirectoryURL).appendingPathExtension("json")
+    
+    
+    @Published var prioritizedTasks: [PrioritizedTasks] = [] {
+        didSet {
+            saveJSONPrioritizedTask()
+        }
+    }
     
     init() {
         loadJSONPrioritizedTasks()
@@ -12,23 +29,13 @@ class TaskStore: ObservableObject {
     prioritizedTasks.firstIndex { $0.priority == priority }!
   }
     
+    
     private func loadJSONPrioritizedTasks() {
         /// print(Bundle.main.bundleURL)
         print(FileManager.documentsDirectoryURL)
         /// let temporaryDirectoryURL = FileManager.default.temporaryDirectory
         /// print(temporaryDirectoryURL)
         
-        // get the URLs for each of the JSON files. For that ou will use bundles URL
-        // Bundle is a representation of your app, its code and resources
-        // All the files you add in xcode to your app target are pakeged up into what is referred to as the app bundle
-        // main represents the current executable, in this case your app
-        // by asking your main bundle for the URL for a given resource and assuming it's located in the bundle, you will get back the URL where the file is located
-        // forResource: is the name of the file you want the URL for
-        // withExtension: is the file extension of the file
-        /// guard let tasksJSONURL = Bundle.main.url(forResource: "PrioritizedTasks", withExtension: "json") else { return }
-        
-        // or the another way:
-        let tasksJSONURL = URL(fileURLWithPath: "PrioritizedTasks", relativeTo: FileManager.documentsDirectoryURL).appendingPathExtension("json")
         
         // Filemanager also provides you with a handy method (contentsOfDirectory) to acquire (для получения) a string array of the contents of a particular directory which will be perfect for knowing what files are available in your different sandbox directories
         // atPath: is a string with the path that to the directory you want to get the contents for
@@ -47,6 +54,31 @@ class TaskStore: ObservableObject {
             print(error)
         }
     }
+    
+    
+    private func saveJSONPrioritizedTask() {
+        // encode your swift type to swift data
+        let encoder = JSONEncoder()
+        
+        // to use a pretty format:
+        encoder.outputFormatting = .prettyPrinted
+        
+        
+        do {
+            // parameter: is a swift object that conforms to the encodable protocol
+            let taskData = try encoder.encode(prioritizedTasks)
+            
+            // specify the URL where to store your data
+            /// let tasksJSONURL = URL(fileURLWithPath: "PrioritizedTasks", relativeTo: FileManager.documentsDirectoryURL).appendingPathExtension("json")
+
+            // write your encoded object to the json file
+            // atomicWrite: is a fancy way of asking that the data be saved to a separate file first, and once that succeeds, it gets exchanged for the fimal file, the one you specified in the URL
+            try taskData.write(to: tasksJSONURL, options: .atomicWrite)
+        } catch let error {
+            print(error)
+        }
+    }
+    
 }
 
 private extension TaskStore.PrioritizedTasks {
