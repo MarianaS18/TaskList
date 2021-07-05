070185a618c1848ed143e21beb58ca2e44e499b8
Mariana <mariana@fastmail.net>
2021-07-05 18:14:10 +0200

    Json decoding

--- TaskList/Models/TaskStore.swift
+++ TaskList/Models/TaskStore.swift
@@ -1,42 +1,41 @@
 import Combine
+import Foundation
 
 class TaskStore: ObservableObject {    
-  @Published var prioritizedTasks = [
-    PrioritizedTasks(
-      priority: .high,
-      names: [
-        "Code a SwiftUI app",
-        "Book an escape room",
-        "Walk the cat",
-        "Pick up heavy things and put them down"
-      ]
-    ),
-    PrioritizedTasks(
-      priority: .medium,
-      names: [
-        "Make karaoke playlist",
-        "Present at iOS meetup group",
-      ]
-    ),
-    PrioritizedTasks(
-      priority: .low,
-      names: [
-        "Climb El Capitan"
-      ]
-    ),
-    PrioritizedTasks(
-      priority: .no,
-      names: [
-        "Learn to make baklava",
-        "Play disc golf in every state",
-        "100 movie reboot marathon"
-      ]
-    )
-  ]
+    @Published var prioritizedTasks: [PrioritizedTasks] = []
+    
+    init() {
+        loadJSONPrioritizedTasks()
+    }
   
   func getIndex(for priority: Task.Priority) -> Int {
     prioritizedTasks.firstIndex { $0.priority == priority }!
   }
+    
+    private func loadJSONPrioritizedTasks() {
+        // get the URLs for each of the JSON files. For that ou will use bundles URL
+        // Bundle is a representation of your app, its code and resources
+        // All the files you add in xcode to your app target are pakeged up into what is referred to as the app bundle
+        // main represents the current executable, in this case your app
+        // by asking your main bundle for the URL for a given resource and assuming it's located in the bundle, you will get back the URL where the file is located
+        // forResource: is the name of the file you want the URL for
+        // withExtension: is the file extension of the file
+        guard let tasksJSONURL = Bundle.main.url(forResource: "PrioritizedTasks", withExtension: "json")
+              else { return }
+        
+        // JSONDecoder helps you decode JSON object into instances of a given data type (instances of task og prioritized task
+        let decoder = JSONDecoder()
+        
+        // you use the data initializer that takes your URL
+        do {
+            let tasksData = try Data(contentsOf: tasksJSONURL)
+            // type: is the type to decode from your data. You are loading a task so using TaskStore.PrioritizedTasks.self (array of) will tell to the method
+            // from: is the JSON object to decode
+            prioritizedTasks = try decoder.decode([TaskStore.PrioritizedTasks].self, from: tasksData)
+        } catch let error {
+            print(error)
+        }
+    }
 }
 
 private extension TaskStore.PrioritizedTasks {
--- TaskList/Views/ContentView.swift
+++ TaskList/Views/ContentView.swift
@@ -26,41 +26,7 @@ struct ContentView: View {
     .sheet(isPresented: $modalIsPresented) {
       NewTaskView(taskStore: self.taskStore)
     }
-    .onAppear() {
-        self.loadJSON()
-    }
   }
-    
-    private func loadJSON() {
-        // get the URLs for each of the JSON files. For that ou will use bundles URL
-        // Bundle is a representation of your app, its code and resources
-        // All the files you add in xcode to your app target are pakeged up into what is referred to as the app bundle
-        // main represents the current executable, in this case your app
-        // by asking your main bundle for the URL for a given resource and assuming it's located in the bundle, you will get back the URL where the file is located
-        // forResource: is the name of the file you want the URL for
-        // withExtension: is the file extension of the file
-        guard let taskJSONURL = Bundle.main.url(forResource: "Task", withExtension: "json"),
-              let prioritizedTaskJSONURL = Bundle.main.url(forResource: "PrioritizedTask", withExtension: "json")
-              else { return }
-        
-        // JSONDecoder helps you decode JSON object into instances of a given data type (instances of task og prioritized task
-        let decoder = JSONDecoder()
-        
-        // you use the data initializer that takes your URL
-        do {
-            let taskData = try Data(contentsOf: taskJSONURL)
-            // type: is the type to decode from your data. You are loading a task so using Task.self will tell to the method
-            // from: is the JSON object to decode
-            let task = try decoder.decode(Task.self, from: taskData)
-            print(task)
-            
-            let prioritizedTaskData = try Data(contentsOf: prioritizedTaskJSONURL)
-            let prioritizedTask = try decoder.decode(TaskStore.PrioritizedTasks.self, from: prioritizedTaskData)
-            print(prioritizedTask)
-        } catch let error {
-            print(error)
-        }
-    }
 }
 
 
