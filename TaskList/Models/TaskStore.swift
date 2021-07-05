18165f471d5e09b1530aa350bb76158b4eccf545
Mariana <mariana@fastmail.net>
2021-07-05 18:39:23 +0200

    saving data on device

--- TaskList/Models/TaskStore.swift
+++ TaskList/Models/TaskStore.swift
@@ -13,6 +13,11 @@ class TaskStore: ObservableObject {
   }
     
     private func loadJSONPrioritizedTasks() {
+        /// print(Bundle.main.bundleURL)
+        print(FileManager.documentsDirectoryURL)
+        /// let temporaryDirectoryURL = FileManager.default.temporaryDirectory
+        /// print(temporaryDirectoryURL)
+        
         // get the URLs for each of the JSON files. For that ou will use bundles URL
         // Bundle is a representation of your app, its code and resources
         // All the files you add in xcode to your app target are pakeged up into what is referred to as the app bundle
@@ -20,8 +25,14 @@ class TaskStore: ObservableObject {
         // by asking your main bundle for the URL for a given resource and assuming it's located in the bundle, you will get back the URL where the file is located
         // forResource: is the name of the file you want the URL for
         // withExtension: is the file extension of the file
-        guard let tasksJSONURL = Bundle.main.url(forResource: "PrioritizedTasks", withExtension: "json")
-              else { return }
+        /// guard let tasksJSONURL = Bundle.main.url(forResource: "PrioritizedTasks", withExtension: "json") else { return }
+        
+        // or the another way:
+        let tasksJSONURL = URL(fileURLWithPath: "PrioritizedTasks", relativeTo: FileManager.documentsDirectoryURL).appendingPathExtension("json")
+        
+        // Filemanager also provides you with a handy method (contentsOfDirectory) to acquire (для получения) a string array of the contents of a particular directory which will be perfect for knowing what files are available in your different sandbox directories
+        // atPath: is a string with the path that to the directory you want to get the contents for
+        print((try? FileManager.default.contentsOfDirectory(atPath: FileManager.documentsDirectoryURL.path)) ?? [])
         
         // JSONDecoder helps you decode JSON object into instances of a given data type (instances of task og prioritized task
         let decoder = JSONDecoder()
